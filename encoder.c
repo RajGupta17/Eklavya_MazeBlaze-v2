9ad98c26b4835eb9b815cf0be3008c1848dad3f5
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-10 15:10:24 +0530

    Update encoder.c
    modifying the first and fixing syntax errors

--- encoder.c
+++ encoder.c
@@ -1,36 +1,41 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "driver/gpio.h"    //for GPIO pins
-#include "esp_attr.h"      //For IRAM - Instant ram where interupts are stored
+#include "freertos/queue.h"
+#include "driver/gpio.h"
 
 #define encoder_phase_A 18
-#define encoder_phase_B 19  //for directly these macros instead of remembering numbers 
+#define encoder_phase_B 19
 
-int count = 0 ;
+volatile int count = 0 ; 
+int direction = 0 ;
 
-void IRAM_ATTR isr() {
-    if(gpio_get_level(encoder_phase_B) == 1) //This symbolizes that B is high when A is RISING
-    {
-        count ++; //clockwise
-        printf("%d\n" , count ) ;
-    }
 
-    else if(gpio_get_level(encoder_phase_B) == 0) //This symbolizes that B is low when A is RISING
-    {
-        count --;  //anticlockwise
-        printf("%d\n" , count ) ;
+static void IRAM_ATTR gpio_isr_handler(void* arg) {
+    if(1) {
+        if(gpio_get_level(encoder_phase_A))
+            direction = gpio_get_level(encoder_phase_B);
+        if(direction) {
+            count++;
+            printf("%d %d" , count , direction ) ;
+        } else {
+            count--;
+            printf("%d %d" , count , direction ) ;
+        }
     }
 }
 
-void setup() {
-	Serial.begin(115200);
-	pinMode(encoder_phase_A, INPUT_PULLUP);
-	attachInterrupt(encoder_phase_A, isr, RISING );
-}
-
-void app main ()
+void app_main ()
 {
-    setup() ;
-    gpio_isr_handler_add(encoder_phase_A, isr, (void*)encoder_phase_B);
+    gpio_config_t conf;
+    conf.intr_type = GPIO_INTR_ANYEDGE;
+    conf.mode = GPIO_MODE_INPUT;
+    conf.pin_bit_mask = (1 << encoder_phase_A ) | (1 << encoder_phase_B);
+    conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    gpio_config(&conf);
+
+    gpio_isr_handler_add(encoder_phase_A, gpio_isr_handler, (void*)encoder_phase_A);
+    gpio_isr_handler_add(encoder_phase_B, gpio_isr_handler, (void*)encoder_phase_B);
+
+    gpio_isr_handler_remove(encoder_phase_A);
+    gpio_isr_handler_remove(encoder_phase_B);
 }
