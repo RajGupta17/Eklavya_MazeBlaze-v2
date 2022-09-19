3db9183158307391c3e14a9d5a506a4238852ba9
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-19 14:49:42 +0530

    Embedded C code
    for Line following , turns etc.

---
+++ Embedded/Lsa_Read.c
@@ -0,0 +1,12 @@
+#include "mazeblaze2.h"
+//this code will print the readings of LSA on terminal
+
+void app_main(void)
+{
+    ESP_ERROR_CHECK() ;
+
+    while (1)
+    {
+        
+    }
+}
---
+++ Embedded/header files/lsa.h
@@ -0,0 +1,11 @@
+#ifndef _LSA_H 
+#define _LSA_H 
+
+#include <stdio.h>
+//// this part of code is commented to avoid error . should be uncommented before use ////
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/adc.h"
+#include "esp_adc_cal.h"
+
+#endif 
