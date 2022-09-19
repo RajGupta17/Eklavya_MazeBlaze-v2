4f84e62f14c3baec5d0bb44b8f205609065dda5b
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-19 15:26:27 +0530

    Update lsa.c
    adc channel 2 doesnt allow pin 0 , 4 15 etc to be used

--- Embedded/header files/lsa.c
+++ Embedded/header files/lsa.c
@@ -20,11 +20,11 @@ esp_err_t enable_lsa()
     flag_5 = adc1_config_channel_atten(ADC1_CHANNEL_7, ADC_ATTEN_DB_11);
     flag_6 = adc1_config_channel_atten(ADC1_CHANNEL_4, ADC_ATTEN_DB_11);
     flag_7 = adc1_config_channel_atten(ADC1_CHANNEL_5, ADC_ATTEN_DB_11);
-    flag_8 = adc2_config_channel_atten(ADC2_CHANNEL_0, ADC_ATTEN_DB_11);
+    flag_8 = adc2_config_channel_atten(ADC2_CHANNEL_12, ADC_ATTEN_DB_11);
     return flag_0 || flag_1 || flag_2 || flag_3 || flag_4 || flag_5 || flag_6 || flag_7 || flag_8; // we use or || because success of flag is 0 here
 }
 
-int get_reading_lsa() // gets raw readings in range of 400 - 2000 appx
+int get_raw_lsa() // gets raw readings in range of 400 - 2000 appx
 {
 
     lsa_reading[0] = adc1_get_raw(ADC1_CHANNEL_0);
@@ -33,11 +33,11 @@ int get_reading_lsa() // gets raw readings in range of 400 - 2000 appx
     lsa_reading[3] = adc1_get_raw(ADC1_CHANNEL_7);
     lsa_reading[4] = adc1_get_raw(ADC1_CHANNEL_4);
     lsa_reading[5] = adc1_get_raw(ADC1_CHANNEL_5);
-    lsa_reading[6] = adc1_get_raw(ADC2_CHANNEL_0);
+    lsa_reading[6] = adc2_get_raw(ADC2_CHANNEL_12);
 
     for (int i = 0; i < no_of_sensors; i++)
     {
-        if (lsa_readings[i] < 1000)
+        if (lsa_readings[i] > 1000)
         {
             lsa_readings[i] = WHITE_PATCH;
         }
@@ -49,3 +49,4 @@ int get_reading_lsa() // gets raw readings in range of 400 - 2000 appx
 
     return lsa_readings ; //returns array of LSA readings 
 }
+
