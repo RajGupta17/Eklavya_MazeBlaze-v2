79fea36ed9b2063a0517e281a342a3c3f3419011
PritK99 <103832825+PritK99@users.noreply.github.com>
2022-09-22 09:19:40 +0530

    To read distance from both encoders separately

---
+++ 4_Encoders/main/main.c
@@ -0,0 +1,82 @@
+#include <stdbool.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "esp_system.h"
+#include "esp_log.h"
+#include "rotary_encoder.h"
+
+#define TAG "app"
+
+#define ROT_ENC_A_GPIO 17
+#define ROT_ENC_B_GPIO 5
+#define ROT_ENC_C_GPIO 4
+#define ROT_ENC_D_GPIO 16
+
+#define ENABLE_HALF_STEPS false  // Set to true to enable tracking of rotary encoder at half step resolution
+#define RESET_AT          0      // Set to a positive non-zero number to reset the position if this value is exceeded
+#define FLIP_DIRECTION    false  // Set to true to reverse the clockwise/counterclockwise sense
+
+void app_main()
+{
+    // esp32-rotary-encoder requires that the GPIO ISR service is installed before calling rotary_encoder_register()
+    ESP_ERROR_CHECK(gpio_install_isr_service(0));
+
+    // Initialise the rotary encoder device with the GPIOs for A and B signals
+    rotary_encoder_info_t info1 = { 0 };
+    rotary_encoder_info_t info2 = { 0 };
+    ESP_ERROR_CHECK(rotary_encoder_init1(&info1, ROT_ENC_A_GPIO, ROT_ENC_B_GPIO));
+    ESP_ERROR_CHECK(rotary_encoder_init2(&info2, ROT_ENC_C_GPIO, ROT_ENC_D_GPIO));
+
+    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&info1, ENABLE_HALF_STEPS));
+    ESP_ERROR_CHECK(rotary_encoder_enable_half_steps(&info2, ENABLE_HALF_STEPS));
+#ifdef FLIP_DIRECTION
+    ESP_ERROR_CHECK(rotary_encoder_flip_direction(&info1));
+    ESP_ERROR_CHECK(rotary_encoder_flip_direction(&info2));
+#endif
+
+    // Create a queue for events from the rotary encoder driver.
+    // Tasks can read from this queue to receive up to date position information.
+    QueueHandle_t event_queue = rotary_encoder_create_queue();
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info1, event_queue));
+    ESP_ERROR_CHECK(rotary_encoder_set_queue(&info2, event_queue));
+
+    while (1)
+    {
+        // Wait for incoming events on the event queue.
+        rotary_encoder_event_t event = { 0 };
+        if (xQueueReceive(event_queue, &event, 1000 / portTICK_PERIOD_MS) == pdTRUE)
+        {
+            ESP_LOGI(TAG, "Event1: Distance1: %lf, direction1 %s,Distance2: %lf, direction2 %s", (event.state.position1)*0.055,
+                     event.state.direction1 ? (event.state.direction1 == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET",(event.state.position2)*0.055,
+                     event.state.direction2 ? (event.state.direction2 == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+        }
+        else
+        {
+            // Poll current position and direction
+            rotary_encoder_state_t state = { 0 };
+            ESP_ERROR_CHECK(rotary_encoder_get_state(&info1, &state));
+            ESP_ERROR_CHECK(rotary_encoder_get_state(&info2, &state));
+            ESP_LOGI(TAG, "Distance1 %lf, direction1 %s,Distance2 %lf, direction2 %s", (state.position1)*0.055,
+                     state.direction1 ? (state.direction1 == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET",(state.position2)*0.055,
+                     state.direction2 ? (state.direction2 == ROTARY_ENCODER_DIRECTION_CLOCKWISE ? "CW" : "CCW") : "NOT_SET");
+
+            // Reset the device
+            if (RESET_AT && (state.position1 >= RESET_AT || state.position1 <= -RESET_AT))
+            {
+                ESP_LOGI(TAG, "Reset");
+                ESP_ERROR_CHECK(rotary_encoder_reset(&info1));
+            }
+            if (RESET_AT && (state.position2 >= RESET_AT || state.position2 <= -RESET_AT))
+            {
+                ESP_LOGI(TAG, "Reset");
+                ESP_ERROR_CHECK(rotary_encoder_reset(&info2));
+            }
+        }
+    }
+    ESP_LOGE(TAG, "queue receive failed");
+
+    ESP_ERROR_CHECK(rotary_encoder_uninit(&info1));
+    ESP_ERROR_CHECK(rotary_encoder_uninit(&info2));
+}
+
