b38694077dbb8b6493d7811c5f4e19464ea3e092
Raj Gupta <98795541+RajGupta17@users.noreply.github.com>
2022-10-07 16:44:39 +0530

    Add files via upload

--- 5_line_following/main/main.c
+++ 5_line_following/main/main.c
@@ -1,223 +1,144 @@
-/*Understanding the flow of code :-
-Here we create two tasks , namely line follow task and turn task simultanously .
-The line follow task starts by getting the raw LSA values and then calculating the correction if any . Then it feeds this correction to motor by passing argument to set_motor_speed() . It also executes turning when a node is spotted .
-The front LSA is used to note that a path exists and the back sensors are used to detect when to take sharp turn */
-
-#include <stdio.h>
 #include "mazeblaze2.h"
-#include "line_following.h"
-#include "tuning_http_server.h"
-
-
-#define GOOD_DUTY_CYCLE 67
+/*This method involves tuning kp , ki ,kd physically*/
+#define GOOD_DUTY_CYCLE 68
 #define MIN_DUTY_CYCLE 55
-#define MAX_DUTY_CYCLE 80
+#define MAX_DUTY_CYCLE 85
+#define NO_OF_NODES 150
 
+/*variables which help to decide which turns to take*/
+bool left_possible ;
+bool right_possible = 0 ; 
+bool straight_possible = 0 ;
 
-int actiavate_left_counter =0;
-int actiavate_right_counter =0;
 int Turn ;
 float error=0, prev_error=0, difference, cumulative_error, correction; 
 float left_duty_cycle = 0, right_duty_cycle = 0;
 int prev_Turn ;
-int prev_lsa1_counter = 0 ; 
-int prev_lsa3_counter = 0 ; 
-bool straight_possible = false ;
-float degree;
+int prev_lsa0_value = 0 ;  //left most sensor
+int prev_lsa2_value = 0 ; //middle sensor
+//we dont need a right sensor prev reading since we are using left follow
+bool is_end = false ;
+int total_angle = 0;
 
-void get_degree();
+int dry_run[NO_OF_NODES] = {0} ;; //To be filled during dry run
+/* Dry_run will hold only 4 types of values , i.e. 1 for West , 3 for North , 5 for East , 7 for South */
+int final_run[NO_OF_NODES] = {3 , 0}; //after removing redundant values from dry run and the first value is hardcoded as 3 since it north as start always
+int degree[NO_OF_NODES] = {0} ; //This contains angles taken at node . hardcoding the first degree as 0 , since it is always going to be in line 
+int degree_index = 1 ; //This contains index for degree array
+int turn_index = 0 ;   //This contains index for turns taken
+int angle ;
+int final_run_index = 1 ; //final_run[0] is already 3 which stands for north 
+int prev_in_final_run;
+
 void circular_defn(int change_in_dir) ;
 float bound(float val, float min, float max) ;
 void calculate_correction() ;
 void calculate_error() ;
 
 
-void turn_task(void *arg)
+void line_follow_task(void *arg)
 {
-    while(1)
+
+    while (1)
     {
-        get_raw_lsa() ;
-
-        /* This part detects if there is a node and if there is , it reduces the speed to pwm 65 and works on counters*/
-        if (lsa_reading[0] == 1) /*A path in left detected once */
-        {
-            if (lsa_reading[1] != prev_lsa1_counter)
-            {
-                actiavate_left_counter ++ ;
-                prev_lsa1_counter = lsa_reading[0] ;
-                get_degree();
-                
-            }
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;//Reduce pwm to 65 to slow down to bot 
-            set_motor_speed(MOTOR_A_1 , MOTOR_FORWARD , 65) ;
-        }
-        if (lsa_reading[4] == 1) /*A path in right detected first*/
-        {
-            if (lsa_reading[3] != prev_lsa3_counter)
-            {
-                actiavate_right_counter ++ ;
-                prev_lsa1_counter = lsa_reading[3] ;
-                get_degree();
-            }
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;//Reduce pwm to 65 to slow down to bot 
-            set_motor_speed(MOTOR_A_1 , MOTOR_FORWARD , 65) ;
-        }
-        if( (actiavate_left_counter > 0 )&& (actiavate_right_counter > 0 )&& ( lsa_reading[1] == 1) && ( lsa_reading[2] == 1 ) && ( lsa_reading[3] == 1 )) /*straight does exsist even though left or right counter are incremented*/
-        {
-            straight_possible = true ;
-            get_degree();
-        }
-
-        /*This part of the code takes action on the basis of the node detected*/
-        if (actiavate_left_counter > 0 && lsa_reading[5] == 1)
-        {
-            circular_defn(-2) ;     //for turning left , we do -2 
-            do
-            {
-                set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ; 
-                set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-                if (lsa_reading[3] != prev_lsa3_counter)
-                    {
-                        actiavate_right_counter = actiavate_left_counter ; 
-                        prev_lsa1_counter = lsa_reading[3] ;
-                        get_degree();
-                    }
-            }
-            while (( lsa_reading[1] == 1) && ( lsa_reading[2] == 1 ) && ( lsa_reading[3] == 1 ) && actiavate_left_counter == 1) ;
-        }
-        else if ( straight_possible )
-        {
-            circular_defn(0) ;      //no change in direction
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 75) ;//Increase PWM since we have to go straight 
-            set_motor_speed(MOTOR_A_1 , MOTOR_FORWARD , 75) ;
-        }
-        else if (actiavate_right_counter > 0 && lsa_reading[6] == 1)
-        {
-            circular_defn(+2) ;     //for turning right , we do +2 
-            do
-            {
-                set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;
-                set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-            }
-            while ( (lsa_reading[1] == 1) && ( lsa_reading[2] == 1 ) && ( lsa_reading[3] == 1 )) ;
-        } 
-        else
-        {
-            circular_defn(-4) ;
-            set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ;//Increase PWM since we have to go straight 
-            set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-            do
-            {
-                set_motor_speed(MOTOR_A_0 , MOTOR_FORWARD , 65) ; 
-                set_motor_speed(MOTOR_A_1 , MOTOR_BACKWARD , 65) ;
-                if (lsa_reading[3] != prev_lsa3_counter)
-                    {
-                        actiavate_right_counter = actiavate_left_counter ; 
-                        prev_lsa1_counter = lsa_reading[3] ;
-                    }
-            }
-            while (( lsa_reading[1] == 1) && ( lsa_reading[2] == 1 ) && ( lsa_reading[3] == 1 ) && actiavate_left_counter == 1) ;
-        }        
-        
-    }
-    vTaskDelete(NULL);
-}
-
-void line_follow_task(void* arg)
-{
-    ESP_ERROR_CHECK(enable_lsa()) ;
-    ESP_ERROR_CHECK(enable_motor_driver()) ;
-    
-    while(1)
-    {
-        get_raw_lsa() ;
+        get_raw_lsa();
 
         calculate_error();
         calculate_correction();
 
-        left_duty_cycle = bound((GOOD_DUTY_CYCLE - correction), MIN_DUTY_CYCLE , MAX_DUTY_CYCLE);
-        right_duty_cycle = bound((GOOD_DUTY_CYCLE + correction), MIN_DUTY_CYCLE , MAX_DUTY_CYCLE);
-
-        set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);
+        left_duty_cycle = bound((GOOD_DUTY_CYCLE - correction), MIN_DUTY_CYCLE, MAX_DUTY_CYCLE);
+        right_duty_cycle = bound((GOOD_DUTY_CYCLE + correction), MIN_DUTY_CYCLE, MAX_DUTY_CYCLE);
+   
+        set_motor_speed(MOTOR_A_0, MOTOR_FORWARD, left_duty_cycle);    /*goes forward in this case*/
         set_motor_speed(MOTOR_A_1, MOTOR_FORWARD, right_duty_cycle);
-
-        /*prints on terminal*/
-        ESP_LOGI("debug","left_duty_cycle:  %f    ::  right_duty_cycle :  %f  :: error :  %f  correction  :  %f  \n",left_duty_cycle, right_duty_cycle, error, correction);
-        ESP_LOGI("error_correction", "KP: %f ::  KI: %f  :: KD: %f", read_pid_const().kp, read_pid_const().ki, read_pid_const().kd);
 
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 
     vTaskDelete(NULL);
 }
+// end of task
 
 void app_main()
 {
-    xTaskCreate(&turn_task, "line_follow_task", 4096, NULL, 1, NULL);    //creating a task to start line following
-    xTaskCreate(&line_follow_task, "line_follow_task", 4096, NULL, 1, NULL);    //creating a task to start line following
-    start_tuning_http_server();    
+    ESP_ERROR_CHECK(enable_lsa());
+    ESP_ERROR_CHECK(enable_motor_driver());
+
+    xTaskCreate(&line_follow_task, "line_follow_task", 4096, NULL, 1, NULL); // creating a task to start line following
 }
-
-/*
-possible cases of errors :-
-1) All the front sensor read black
-2) if lsa_reading [1] != lsa_reading [2] != lsa_reading [3]
-
-error to right of line is positive while left of line is negative
-*/
-
-// void turns(void *args)
-// {
-//     turns();
-// }
+// end of main
 
 void calculate_error()
 {
-    if (lsa_reading[1] == 0 && lsa_reading[2] == 0 && lsa_reading[3] == 0 )
-    {
+    /*
+    possible cases of errors :-
+    1) All the front sensor read black - this means the bot is entirely out of path
+    2) if lsa_reading [1] != lsa_reading [2] != lsa_reading [3] != 1 - this means bot is slightly out of path
 
-        if(prev_error > 0) //we use prev_error to extract the sign to give to error
+    error to right of line is +ve while left of line is -ve
+    */
+    if (lsa_reading[0] == 0 && lsa_reading[1] == 0 && lsa_reading[2] == 0 && lsa_reading[3] == 0 && lsa_reading[4] == 0)
+    {
+        /*All sensore read black*/
+        if (prev_error > 0) // we use prev_error to extract the sign to give to error
         {
-            error = 2.5;    //arbitrary value
+            error = 2.5; // arbitrary value
         }
-        else 
+        else
         {
-            error = -2.5;   //arbitrary value
+            error = -2.5; // arbitrary value
         }
     }
     else if (lsa_reading[1] == 0 && lsa_reading[3] == 1000)
     {
         /* turn right to nullify */
-        error = 1 ;     //arbitrary value
+        error = 1; // arbitrary value
     }
     else if (lsa_reading[1] == 1000 && lsa_reading[3] == 0)
     {
         /* turn left to nullify */
-        error = -1 ;    //arbitrary value
+        error = -1; // arbitrary value
     }
     else if (lsa_reading[1] == 1000 && lsa_reading[2] == 1000 && lsa_reading[3] == 1000)
     {
-        /* no error */
-        error = 0 ;     //arbitrary value
+        /* no error since the bot is on line */
+        error = 0; // arbitrary value
     }
     else
-    { 
-        error = prev_error ; //this case is for safety
-    }   
+    {
+        error = 0; // this case is for safety
+    }
 }
+// end of function
 
 void calculate_correction()
 {
-    error = error*10;  // we need the error correction in range 0-100 so that we can send it directly as duty cycle paramete
-    difference = error - prev_error;    //used for calcuating kd 
-    cumulative_error += error;     //used for calculating ki
+    error = error * 10;              // we need the error correction in range 0-100 so that we can send it directly as duty cycle paramete
+    difference = error - prev_error; // used for calcuating kd
+    cumulative_error += error;       // used for calculating ki
 
-    cumulative_error = bound(cumulative_error, -30, 30); //bounding cumulative_error to avoid the issue of cumulative_error being very large
+    cumulative_error = bound(cumulative_error, -30, 30); // bounding cumulative_error to avoid the issue of cumulative_error being very large
 
-    correction = read_pid_const().kp*error + read_pid_const().ki*cumulative_error + read_pid_const().kd*difference; //defined in http_server.c
+    float kp , ki , kd ;
+    kp = 3.5 ;
+    kd = 1.25 ;
+    ki = 0 ; 
 
-    prev_error = error; //update error
+    correction = kp* error + ki * cumulative_error + kd * difference; // defined in http_server.c
+
+    prev_error = error; // update error
 }
+// end of function
+
+float bound(float val, float min, float max) //To bound a certain value in range MAX to MIN 
+{
+    if (val > max)
+        val = max;
+    else if (val < min)
+        val = min;
+    return val;
+}
+//end of function
 
 void circular_defn(int change_in_dir)
 {
@@ -242,15 +163,4 @@ void circular_defn(int change_in_dir)
         now 9 - 8 is 1 which is 1 . Now using LFR you are facing west which is also 1*/
     }
 }
-
-float bound(float val, float min, float max) //To bound a certain value in range MAX to MIN 
-{
-    if (val > max)
-        val = max;
-    else if (val < min)
-        val = min;
-    return val;
-}
-
-
-
+//end of function
